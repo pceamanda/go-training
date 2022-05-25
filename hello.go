649734ffaa335a93b65d449eb5a4d76a07ab8315
prado.amanda <prado.amanda@ifood.com.br>
2022-05-25 12:54:34 -0300

    adding net http

--- hello.go
+++ hello.go
@@ -1,26 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
 func main() {
-	var name string
-	var age int
 
-	fmt.Println("Digite seu nome")
-	fmt.Scan(&name)
+	intro()
+	for {
+		menu()
 
-	fmt.Println("Digite sua idade")
-	fmt.Scan(&age)
+		command := readCommand()
+
+		switch command {
+		case 1:
+			monitoring()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+	}
+}
+
+func getNameAndAge() (string, int) {
+	name := "Amanda"
+	age := 28
+	return name, age
+}
+
+func intro() {
+
+	name, age := getNameAndAge()
 
 	version := 1.1
 	fmt.Println("Olá, sra.", name, "sua idade é", age)
 	fmt.Println("Este programa está na versão", version)
+}
 
+func readCommand() int {
+	var command int
+	fmt.Scan(&command)
+	fmt.Println("O comando escolhido foi", command)
+	return command
+}
+
+func menu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
+}
 
-	var command int
-	fmt.Scan(&command)
-
-	fmt.Println("O comando escolhido foi", command)
+func monitoring() {
+	fmt.Println("Monitorando...")
+	site := "https://alura.com.br/askjdbahsbciahsbca"
+	response, _ := http.Get(site)
+	
+	if response.StatusCode == 200 {
+        fmt.Println("Site:", site, "foi carregado com sucesso!")
+    } else {
+        fmt.Println("Site:", site, "está com problemas. Status Code:", response.StatusCode)
+    }
 }
